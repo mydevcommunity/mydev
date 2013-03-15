4077b6f1f7d06c736b03018d25d65cd8d595f1c5
Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2013-03-15 16:12:55 +0800

    fix settings

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -12,14 +12,14 @@ TIMEZONE = 'Asia/Kuala_Lumpur'
 
 DEFAULT_LANG = u'en'
 
-# Blogroll
-# LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-#           ('Python.org', 'http://python.org'),
-#           ('Jinja2', 'http://jinja.pocoo.org'),
-#           ('You can modify those links in your config file', '#'),)
+# Blogroll - This is required
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+           ('Python.org', 'http://python.org'),
+           ('Jinja2', 'http://jinja.pocoo.org'),
+           ('You can modify those links in your config file', '#'),)
 
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
+# Social widget - This is required
+SOCIAL = (('You can add links in your config file', '#'),
+           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
